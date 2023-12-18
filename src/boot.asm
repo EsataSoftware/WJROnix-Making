b5a541e87a590cbe91f142f852d4eaa01d0638a8
Ww2815415971 <2815415971@qq.com>
2023-12-18 20:55:06 +0800

    basic eniverment and knowledge

---
+++ src/boot.asm
@@ -0,0 +1,34 @@
+[org 0x7c00]
+
+;设置屏幕模式为文本模式：清除屏幕
+mov ax, 3
+int 0x10
+;初始化段寄存器
+mov ax, 0
+mov ds, ax
+mov es, ax
+mov ss,ax
+mov sp, 0x7c00
+xchg bx,bx;bochs 的魔术断点
+mov si,booting 
+call print
+;阻塞
+   jmp $
+print:
+   mov ah, 0x0e
+.next:
+   mov  al,[si]
+   cmp al, 0
+   jz .done
+   int 0x10
+   inc si
+   jmp .next
+.done:
+   ret
+
+booting:
+    db "Booting Onix...", 10, 13, 0;\n \r
+;填充
+ times 510-($ - $$) db 0
+;主引导扇区规定最后两个字节必须是0x55，0xaa
+ db 0x55, 0xaa
