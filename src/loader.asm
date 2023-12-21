8e091a890ab4242e84d924e99e2e2e69f16e441c
Ww2815415971 <2815415971@qq.com>
2023-12-21 21:27:41 +0800

    12.21日内存检测提交

--- src/loader.asm
+++ src/loader.asm
@@ -2,9 +2,27 @@
 
   dw 0x55aa;来判断主引导扇区是否发生错误
 
-xchg bx,bx;
+
   mov si ,loading
   call print  
+xchg bx,bx;
+
+detect_memory:
+   ;寄存器赋值
+   xor ebx ,ebx;将ebx赋0
+   ;确定缓冲区的大小
+   mov ax,0;
+   mov es,ax
+   mov edi,ards_buffer;
+
+   mov edx ,0x534d4150;固定签名
+
+   .next
+      ;子功能号
+      mov eax,0xe820
+      ;结构体的大小（字节）
+      mov ecx ,20
+
   ;显示loading
   print:
    mov ah, 0x0e
@@ -20,3 +38,17 @@ xchg bx,bx;
 
 loading:
     db "Loading Onix...", 10, 13, 0;\n \r
+detecting:
+    db "Detecting Memory Success...", 10, 13, 0;\n \r
+ error:
+   mov si,.msg
+   call print
+   hlt;cpu停止
+   jmp $
+   .msg db "Loading Error...", 10, 13, 0;\n \r   
+
+
+   ards_count:;ards大小
+      dw 0
+   ards_buffer:;ards缓冲区，不固定值是因为不同的虚拟机的buffer不同
+
