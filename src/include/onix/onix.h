c5fa1cf4a74af919e813fd431889e3dad992eb4e
Ww2815415971 <2815415971@qq.com>
2023-12-30 21:59:46 +0800

    使用gcc进行编译与链接

---
+++ src/include/onix/onix.h
@@ -0,0 +1,7 @@
+# ifndef ONIX_H
+# define ONIX_H
+
+# define ONIX_MAGIC 20231230
+
+void kernel_init();
+# endif
---
+++ src/kernel/main.c
@@ -0,0 +1,17 @@
+# include <onix/onix.h>
+
+int magic= ONIX_MAGIC;
+
+char message[]="Hello world!!!";//初始化的数据放在data中
+char buf[1024];//未初始化的数据放在bss中
+
+void kernel_init()
+{
+    char * vedeo=(char *)0xb8000;//指向文本显示框
+    for (int  i = 0; i < sizeof(message); i++)
+    {
+        vedeo [i*2]=message[i];
+    }
+    
+}
+
