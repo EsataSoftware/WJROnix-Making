09480e0c54400052af609b1ecb43911da66a94c4
Ww2815415971 <2815415971@qq.com>
2024-01-22 20:55:13 +0800

    types.h与type.c的连接调试

--- tests/types.c
+++ tests/types.c
@@ -1,9 +1,9 @@
-#include <onix/types.h>
+#include "onix/types.h"
 #include <stdio.h>
 
 typedef struct descriptor /* 共8位*/
     {
-    unsigned short limit_low;//段界限0~15位
+    unsigned short limit_low;//段界限0~15位s
     unsigned int base_low : 24 ;//基地址0~23位16M;
     unsigned char type : 4;//段类型
     unsigned char segment : 1;//1·表示代码段或数据段，0 表示系统段;
@@ -15,7 +15,7 @@ typedef struct descriptor /* 共8位*/
     unsigned char big :1;//32位还是16位;
     unsigned char granularity :1;// 粒度 4KB 或1B
     unsigned  char base_high;//基地址 24~31 位
-    }__attribute__((packed)) descriptor ;
+    }_packed descriptor ;
 int main()
 {
     printf("size of u8 %d",sizeof(u8));
